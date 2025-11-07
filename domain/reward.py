0ba475d5e373216ec59ba384b55cb2f07eb7e0b4
Thomas HUYNH <thomashuynh795@gmail.com>
2025-11-07 02:40:12 +0100

    2025-11-07 01:40:12 UTC

---
+++ domain/reward.py
@@ -0,0 +1,7 @@
+class Rewards:
+    REWARD_WALL: int = -10
+    REWARD_DEFAULT: int = -1
+    REWARD_KEY: int = 10
+    REWARD_GOAL: int = 1000
+    REWARD_OUT: int = -10
+    REWARD_MONSTER: int = -100
